1143de1e38eef26946ea1d8e1b374f5a19817527
Ganline <hayak.311.gungam.8810@gmail.com>
2022-02-03 20:01:32 +0900

    ロケット破壊時の処理を追加
    破壊した時に爆発エフェクトを付与

--- AR-Rockets/Assets/Script/shootscript.cs
+++ AR-Rockets/Assets/Script/shootscript.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class shootscript : MonoBehaviour
 {
 
+    public GameObject Explosion;
+
     public void Shoot(Vector3 dir) //Ball飛ばす
     {
 
         GetComponent<Rigidbody>().AddForce(dir);
-        Destroy(gameObject, 7);
+        Destroy(gameObject, 3);
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+        Vector3 set = this.gameObject.transform.position;
+        Instantiate(Explosion, set, Quaternion.identity);
     }
 
 }
